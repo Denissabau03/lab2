8bc871307909bd01ab9c2a215c3be05e3b69d5eb
alexu <alexu@DENIS>
2024-10-23 18:59:25 +0300

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace lab2.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Price { get; set; }
+    }
+}
